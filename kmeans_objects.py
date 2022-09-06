24f21ecab289cbd0c1d56de7adff47337b38974d
Mathnar <42184321+Mathnar@users.noreply.github.com>
2022-09-07 01:01:17 +0200

    Initial commit

---
+++ kmeans_objects.py
@@ -0,0 +1,10 @@
+
+
+class k_means_serial(object):
+    def __init__(self, n_clusters):
+        self.num_of_cluster = n_clusters
+
+
+class kmeans_parallel(object):
+    def __init__(self, n_clusters):
+        self.num_of_cluster = n_clusters
