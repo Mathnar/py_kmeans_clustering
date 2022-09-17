87f148c66b8c178a80353a68cab3a55b660bb7ab
Mathnar <42184321+Mathnar@users.noreply.github.com>
2022-09-17 15:46:48 +0200

    iterate on incrementala number of samples

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sklearn.datasets as skl
 import pandas as pd
 
 ###################################
+N = 100 #numero di exe timeit
 n_clusters = '3'
 max_iter = '300'
 blob_samples = '500'
@@ -12,10 +13,10 @@ blob_random_state = '0'  # default is false
 ###################################
 
 
-setup = """
-import sklearn.datasets as skl
-blobs = skl.make_blobs(n_samples=""" + blob_samples + """, centers= """ + bloc_n_features + """, cluster_std= """ + blob_cluster_std + """, random_state= """ + blob_random_state + """)[0]
-print('blobs ', blobs)"""
+def setupp(blob_samples):
+    setup = """import sklearn.datasets as skl; blobs = skl.make_blobs(n_samples=""" + blob_samples + """, centers= """ + bloc_n_features + """, cluster_std= """ + blob_cluster_std + """, random_state= """ + blob_random_state + """)[0]; print('blobss', """+ blob_samples + """)"""
+    return setup
+
 
 stmt = '''
 from kmeans_objects import k_means_serial
@@ -23,8 +24,12 @@ kmeans = k_means_serial(n_clusters =''' + n_clusters + ''', max_iter = ''' + max
 kmeans.fit(blobs)
 '''
 
+k_means_run_serial_t = []
+for i in range(1,6):
+    blob_samples = str(int(blob_samples) + 500)
+    print('\nblob qui ', blob_samples)
+    setup = setupp(blob_samples)
+    k_means_run_serial_t.append(timeit.timeit(stmt=stmt, setup=setup, number=N) / N)
 
-k_means_run_serial = timeit.timeit(stmt=stmt, setup=setup, number=300) / 300
-
-result = pd.DataFrame(k_means_run_serial)
-print('\nresult ', result)
+result = pd.DataFrame(k_means_run_serial_t)
+print('\nresult ', result)  # tempo medio del set di esecuzioni timeit (N) per ogni round
