ab81eb07cb15efe8865e7736a45462c47daab016
Mathnar <42184321+Mathnar@users.noreply.github.com>
2022-09-17 15:04:40 +0200

    sequential clustering timeit execution

--- kmeans_objects.py
+++ kmeans_objects.py
@@ -1,10 +1,52 @@
+import numpy as np
 
 
 class k_means_serial(object):
-    def __init__(self, n_clusters):
+    def __init__(self, n_clusters, max_iter):
         self.num_of_cluster = n_clusters
+        self.max_iter = max_iter
+
+    def min_cluster_dist(self, clusters, x):
+        return np.argmin([self.euc_dist(x, c) for c in clusters])
+
+    def euc_dist(self, a, b):
+        return np.sqrt(((a - b) ** 2).sum())
+
+    def points_clustering(self, blobs):     # clustering
+        self.cluster_prediction = [self.min_cluster_dist(self.cluster_centers_, x) for x in blobs]
+        ind = []
+        for j in range(self.num_of_cluster):
+            current_cluster = []
+            for i, k in enumerate(self.cluster_prediction):
+                if k == j:
+                    current_cluster.append(i)
+            ind.append(current_cluster)
+        points_for_cluster = [blobs[i] for i in ind]
+        return points_for_cluster
+
+    def random_centroids(self, blobs):
+        centroid = np.random.permutation(blobs.shape[0])[:self.num_of_cluster]
+        return blobs[centroid]
+
+    def fit(self, blobs):
+        self.cluster_centers_ = self.random_centroids(blobs)
+        for i in range(self.max_iter):
+            X_by_cluster = self.points_clustering(blobs)
+
+            new_cl_centers = [c.sum(axis=0) / len(c) for c in X_by_cluster] # new clsuter centers
+            new_cl_centers = [arr.tolist() for arr in new_cl_centers]
+            old_centers = self.cluster_centers_
+
+            if np.all(new_cl_centers == old_centers): # check convergence
+                self.number_of_iter = i
+                break
+            else:
+                self.cluster_centers_ = new_cl_centers # keep iter
+        return self
 
 
 class kmeans_parallel(object):
     def __init__(self, n_clusters):
         self.num_of_cluster = n_clusters
+
+
--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from kmeans_objects import *
+import timeit
+import sklearn.datasets as skl
+import pandas as pd
+
+###################################
+n_clusters = '3'
+max_iter = '300'
+blob_samples = '500'
+bloc_n_features = '3'
+blob_cluster_std = '0.6'  # default is 1
+blob_random_state = '0'  # default is false
+###################################
 
 
-def main():
-    print("Hello World!")
+setup = """
+import sklearn.datasets as skl
+blobs = skl.make_blobs(n_samples=""" + blob_samples + """, centers= """ + bloc_n_features + """, cluster_std= """ + blob_cluster_std + """, random_state= """ + blob_random_state + """)[0]
+print('blobs ', blobs)"""
+
+stmt = '''
+from kmeans_objects import k_means_serial
+kmeans = k_means_serial(n_clusters =''' + n_clusters + ''', max_iter = ''' + max_iter + ''')
+kmeans.fit(blobs)
+'''
 
 
-if __name__ == "__main__":
-    main()
+k_means_run_serial = timeit.timeit(stmt=stmt, setup=setup, number=300) / 300
+
+result = pd.DataFrame(k_means_run_serial)
+print('\nresult ', result)
