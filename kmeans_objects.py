1eae7755a7ed7081752f84b04d0684742e643622
Mathnar <42184321+Mathnar@users.noreply.github.com>
2022-09-21 18:03:51 +0200

    parallel first test

--- kmeans_objects.py
+++ kmeans_objects.py
@@ -1,4 +1,5 @@
 import numpy as np
+from multiprocessing import Pool
 
 
 class k_means_serial(object):
@@ -13,7 +14,7 @@ class k_means_serial(object):
         return np.sqrt(((a - b) ** 2).sum())
 
     def points_clustering(self, blobs):     # clustering
-        self.cluster_prediction = [self.min_cluster_dist(self.cluster_centers_, x) for x in blobs]
+        self.cluster_prediction = [self.min_cluster_dist(self.cluster_centers, x) for x in blobs]
         ind = []
         for j in range(self.num_of_cluster):
             current_cluster = []
@@ -29,24 +30,60 @@ class k_means_serial(object):
         return blobs[centroid]
 
     def fit(self, blobs):
-        self.cluster_centers_ = self.random_centroids(blobs)
+        self.cluster_centers = self.random_centroids(blobs)
         for i in range(self.max_iter):
             X_by_cluster = self.points_clustering(blobs)
 
             new_cl_centers = [c.sum(axis=0) / len(c) for c in X_by_cluster] # new clsuter centers
             new_cl_centers = [arr.tolist() for arr in new_cl_centers]
-            old_centers = self.cluster_centers_
+            old_centers = self.cluster_centers
 
             if np.all(new_cl_centers == old_centers): # check convergence
                 self.number_of_iter = i
                 break
             else:
-                self.cluster_centers_ = new_cl_centers # keep iter
+                self.cluster_centers = new_cl_centers # keep iter
         return self
 
 
-class kmeans_parallel(object):
-    def __init__(self, n_clusters):
-        self.num_of_cluster = n_clusters
+class k_means_parallel(k_means_serial):
+    def __init__(self, n_clusters, max_iter, num_of_cores):
+        super().__init__(n_clusters, max_iter)
+        self.num_cores = num_of_cores
 
+    def shuffle(self, list_in, n):
+        temp = np.random.permutation(list_in)
+        result = [temp[i::n] for i in range(n)]
+        return result
 
+    def fit(self, blobs):
+        self.cluster_centers = self.random_centroids(blobs)
+        for i in range(self.max_iter):
+
+            splitted_data = self.shuffle(blobs, self.num_cores)
+
+            pool = Pool()
+            result = pool.map(self.points_clustering, splitted_data)
+            pool.close()
+            pool.join()
+            # splitted e riunito
+
+            points_for_cluster = []
+            for k in range(0, self.num_of_cluster):
+                points = []
+                for pool in range(0, self.num_cores):
+                    tmp = result[pool][k].tolist()
+                    points = sum([points, tmp], [])
+                points_for_cluster.append(np.array(points))
+
+            new_cl_centers = [k.sum(axis=0) / len(k) for k in points_for_cluster]
+            new_cl_centers = [np.array(arr) for arr in new_cl_centers]
+            old_cl_centers = self.cluster_centers
+            old_cl_centers = [np.array(arr) for arr in old_cl_centers]
+            if all([np.allclose(x, y) for x, y in zip(old_cl_centers, new_cl_centers)]):
+                self.number_of_iter = i
+                break
+            else:
+                self.cluster_centers = new_cl_centers
+        self.number_of_iter = i
+        return self
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
+import time
 import timeit
-import sklearn.datasets as skl
 import pandas as pd
 
-###################################
-N = 100 #numero di exe timeit
-n_clusters = '3'
-max_iter = '300'
-blob_samples = '500'
-bloc_n_features = '3'
-blob_cluster_std = '0.6'  # default is 1
-blob_random_state = '0'  # default is false
-###################################
+
+SERIAL = 0
+PARALLEL = 1
 
 
 def setupp(blob_samples):
@@ -18,18 +12,55 @@ def setupp(blob_samples):
     return setup
 
 
-stmt = '''
-from kmeans_objects import k_means_serial
-kmeans = k_means_serial(n_clusters =''' + n_clusters + ''', max_iter = ''' + max_iter + ''')
-kmeans.fit(blobs)
-'''
+if SERIAL:
+    ###################################
+    N = 100 #numero di exe timeit
+    n_clusters = '3'
+    max_iter = '300'
+    blob_samples = '500'
+    bloc_n_features = '3'
+    blob_cluster_std = '0.6'  # default is 1
+    blob_random_state = '0'  # default is false
+    ###################################
 
-k_means_run_serial_t = []
-for i in range(1,6):
-    blob_samples = str(int(blob_samples) + 500)
-    print('\nblob qui ', blob_samples)
+
+    print('\nSerial starting ')
+    time.sleep(1)
+
+
+    stmt = '''from kmeans_objects import k_means_serial; kmeans = k_means_serial(n_clusters =''' + n_clusters + ''', max_iter = ''' + max_iter + '''); kmeans.fit(blobs)'''
+
+    k_means_run_serial_t = []
+    for i in range(1,6):
+        blob_samples = str(int(blob_samples) + 500)
+        print('\nblob qui ', blob_samples)
+        setup = setupp(blob_samples)
+        k_means_run_serial_t.append(timeit.timeit(stmt=stmt, setup=setup, number=N) / N)
+
+    result = pd.DataFrame(k_means_run_serial_t)
+    print('\nresult ', result)  # tempo medio del set di esecuzioni timeit (N) per ogni round
+
+
+    print('\nParallel starting ')
+    time.sleep(5)
+
+if PARALLEL:
+    ###################################
+    N = 100 #numero di exe timeit
+    n_clusters = '3'
+    max_iter = '300'
+    blob_samples = '500'
+    bloc_n_features = '3'
+    blob_cluster_std = '0.6'  # default is 1
+    blob_random_state = '0'  # default is false
+    num_of_cores = '2'
+    ###################################
+
+    stmt = '''from kmeans_objects import k_means_parallel; kmeans = k_means_parallel(n_clusters =''' + n_clusters + ''', max_iter = ''' + max_iter + ''', num_of_cores = ''' + num_of_cores + '''); kmeans.fit(blobs)'''
+
+    k_means_run_parallel_t = []
+
     setup = setupp(blob_samples)
-    k_means_run_serial_t.append(timeit.timeit(stmt=stmt, setup=setup, number=N) / N)
+    k_means_run_parallel_t.append(timeit.timeit(stmt=stmt, setup=setup, number=N) / N)
 
-result = pd.DataFrame(k_means_run_serial_t)
-print('\nresult ', result)  # tempo medio del set di esecuzioni timeit (N) per ogni round
+    print('k_means_run_parallel_t ', k_means_run_parallel_t)
